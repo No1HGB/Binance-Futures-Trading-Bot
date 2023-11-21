585c56e364a6008588636137db8a99780ea1b9a9
HGB <hsw8147@gmail.com>
2023-11-22 02:16:43 +0900

    feat: basic config

--- LiveTradingConfig.py
+++ LiveTradingConfig.py
@@ -1,5 +1,10 @@
-API_KEY = ''
-API_SECRET = ''
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+API_SECRET = os.getenv("API_SECRET")
 
 trading_strategy = 'tripleEMAStochasticRSIATR'
 '''
